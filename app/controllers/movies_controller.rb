55f685229a23ada2e33c5f7f793e64339adf4ce9
kennyhei <kenny_heinonen@hotmail.com>
2013-12-25 21:39:43 +0200

    Update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  #before_filter :authenticate, only: [:create, :new, :destroy]
+  before_filter :authenticate, only: [:create, :new, :destroy]
 
   # GET /movies
   # GET /movies.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,15 @@
 
 d1 = Director.create :name => "Danny Boyle", :birthday => Date.new(1956, 10, 20)
 d2 = Director.create :name => "James Wan", :birthday => Date.new(1977, 2, 27)
+d3 = Director.create :name => "Clint Eastwood", :birthday => Date.new(1930, 5, 31)
 
 m1 = Movie.new :name => "127 Hours", :year => 2010, :runtime => 94, :studio => "Fox Searchlight Pictures"
 m2 = Movie.new :name => "Saw", :year => 2004, :runtime => 102, :studio => "Lionsgate"
+m3 = Movie.new :name => "Gran Torino", :year => 2008, :runtime => 116, :studio => "Warner Bros."
 
 d1.movies << m1
 d2.movies << m2
+d3.movies << m3
 
 m1.roles.create :name => "Aron Ralston"
 
